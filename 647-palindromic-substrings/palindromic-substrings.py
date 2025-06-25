81912f72acf97840fe044a0aed09ac4aaf2c866f
laney522 <62184014+laney522@users.noreply.github.com>
2025-06-25 17:57:16 -0400

    Time: 170 ms (35.24%) | Memory: 17.8 MB (51.23%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -0,0 +1,23 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        count = len(s)
+        #check for odd length palindrome
+        for i in range(len(s)):
+            left = i-1
+            right = i+1
+            while (left >= 0 and left<len(s)) and (right >= 0 and right < len(s) and s[left]==s[right]):
+                count += 1
+                left -= 1
+                right += 1
+
+        #check for even length palindrome
+        for i in range(len(s)):
+            left = i
+            right = i+1
+            while (left >= 0 and left<len(s)) and (right >= 0 and right < len(s) and s[left]==s[right]):
+                count += 1
+                left -= 1
+                right += 1
+
+        return count
+        
